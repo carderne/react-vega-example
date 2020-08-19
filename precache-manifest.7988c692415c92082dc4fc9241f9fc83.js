8d1f2904413daabd114718dc81f9c8b18523535a
Ben Pyrik <ben@wsundine.com>
2020-08-18 20:53:25 -0500

    Updates

---
+++ precache-manifest.7988c692415c92082dc4fc9241f9fc83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18ee1d7f30515c91d74638cebdfc9a2",
+    "url": "/react-vega-example/index.html"
+  },
+  {
+    "revision": "e5127c0f32512a6478c2",
+    "url": "/react-vega-example/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "90883a80fd88bf644676",
+    "url": "/react-vega-example/static/js/2.4eb7f14d.chunk.js"
+  },
+  {
+    "revision": "e2a29c63a5962238e7b569ac4c5c4c44",
+    "url": "/react-vega-example/static/js/2.4eb7f14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5127c0f32512a6478c2",
+    "url": "/react-vega-example/static/js/main.b9f784c9.chunk.js"
+  },
+  {
+    "revision": "5bf1a1a1581750aaeec6",
+    "url": "/react-vega-example/static/js/runtime-main.2cd26741.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4cb4501dfbe96ea380763e04b7d7a7a.js"
+  "/react-vega-example/precache-manifest.7988c692415c92082dc4fc9241f9fc83.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-vega-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
